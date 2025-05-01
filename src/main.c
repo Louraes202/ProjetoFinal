9b95d8a7ea5f5b6864d8d758741fb73e22d68475
Louraes202 <peterlouraes@gmail.com>
2025-05-01 16:52:23 +0100

    Fase 1 - Retificaçoes 2

--- src/main.c
+++ src/main.c
@@ -3,28 +3,11 @@
 
 #include "data.h"
 #include "read_files.h"
+#include "operations.h"
 
 #ifdef _WIN32
   #include <windows.h>   // SetConsoleCP, SetConsoleOutputCP
 #endif
-
-
-// Imprime a lista de distâncias
-void imprimirListaDistancias(NodeDistancia* lista) {
-    printf("\n--- Lista de Distâncias ---\n");
-    if (!lista) {
-        printf("Nenhuma distância carregada.\n");
-        return;
-    }
-    int i = 1;
-    for (NodeDistancia* p = lista; p; p = p->next, i++) {
-        printf("%2d) %d -> %d : %.3f km\n",
-               i,
-               p->distancia.idSensor1,
-               p->distancia.idSensor2,
-               p->distancia.distancia);
-    }
-}
 
 // Liberta a memória da lista de distâncias
 void libertarListaDistancias(NodeDistancia** lista) {
